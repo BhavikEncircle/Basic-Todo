e9686a64c8e327d4176b38e51a222d46fcf08e09
BhavikEncircle <bhavikp.encircle@gmail.com>
2026-01-06 16:01:24 +0530

    added all files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Todo_form from "./Todo_form";
+
+const App = () => {
+  return (
+    <div className="main-container">
+      <h2>Basic Todo </h2>
+      <Todo_form />
+    </div>
+  );
+};
+
+export default App;
